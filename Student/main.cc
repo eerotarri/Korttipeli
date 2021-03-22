2d4e7e54717d3a343b04d1830e90f7116ea965d4
Niilo Klapuri <niilo.klapuri@tuni.fi>
2021-03-22 15:29:29 +0200

    mainwindow näkyy

--- Student/main.cc
+++ Student/main.cc
@@ -1,8 +1,11 @@
 #include <QApplication>
+#include <simplemainwindow.hh>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    SimpleMainWindow w;
+    w.show();
 
     return a.exec();
 }
